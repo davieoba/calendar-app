d0dd6dcf41d19be8dee7ee24ef35fd97f2046214
davieoba <bodunrindavidbond@gmail.com>
2023-05-05 22:48:28 +0100

    hide/show the element when I click on it

--- src/components/calendar/singleCalendar.jsx
+++ src/components/calendar/singleCalendar.jsx
@@ -1,12 +1,42 @@
-import { Calendar } from 'react-date-range'
+import { useState } from 'react'
+import { DateRange } from 'react-date-range'
 
 export const SingleCalendar = () => {
+  const [open, setOpen] = useState(false)
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ])
+
+  console.log({ state })
+
   return (
     <div className='container'>
-      <Calendar className='calendarStyle' />
+      <div className='element-container'>
+        <input
+          type='text'
+          className='single-calendar-input'
+          onClick={() => {
+            setOpen(!open)
+          }}
+        />
 
-      <div>
-        <input type='text' className='single-calendar-input' />
+        <div className={`dropdown ${open ? 'open' : 'inactive'}`}>
+          <DateRange
+            className='calendarStyle'
+            onChange={(item) => setState([item.selection])}
+            ranges={state}
+            showSelectionPreview={false}
+            editableDateInputs={false}
+            showMonthAndYearPickers={false}
+            showDateDisplay={false}
+            rangeColors={['#4C988E', '#4C988E', '#000000']}
+            direction='horizontal'
+          />
+        </div>
       </div>
     </div>
   )
